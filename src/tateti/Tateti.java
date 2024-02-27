f85781177d6bf1995afeb4d306327588d5ce1012
martinLisi82ORT <martin.lisi1212@gmail.com>
2024-02-26 21:45:16 -0300

    Subir a Repositorio

---
+++ src/tateti/Tateti.java
@@ -0,0 +1,18 @@
+package tateti;
+
+import tateti.igu.Vista;
+
+public class Tateti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Vista vista = new Vista();
+        vista.setVisible(true);
+        vista.setLocationRelativeTo(null);
+
+    }
+
+}
